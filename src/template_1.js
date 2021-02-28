55d8adda3633790901b24506e555265eca9e7fcf
Laura Micheller Peterson <michellepeterson.mishmish@gmail.com>
2021-02-28 16:27:45 -0700

    added to template html and css

---
+++ src/template_1.js
@@ -0,0 +1,10 @@
+
+module.exports = templateData => {
+    // destructure page data by section
+    const person = templateData.personData;
+    const projectArr = templateData.projects;
+
+    return `
+    
+    `;
+};
