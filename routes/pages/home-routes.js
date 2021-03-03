b3c7714cde8c454e40f1078997c3fb2f05afd1f8
Mason Howe <mason.a.howe@gmail.com>
2021-03-03 06:28:07 -0700

    add prior portfolio rout

--- routes/pages/home-routes.js
+++ routes/pages/home-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { User, UserContent } = require("../../models");
 
 router.get("/", (req, res) => {
   res.render("home");
@@ -10,6 +11,24 @@ router.get("/login", (req, res) => {
 
 router.get("/dashboard", (req, res) => {
   res.render("dashboard");
+});
+
+router.get("/portfolios", (req, res) => {
+  UserContent.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+  })
+    .then((data) => {
+      //res.json(data);
+      const info = data.map((info) => info.get({ plain: true }));
+      console.log(info);
+      res.render("portfolios", { info });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
