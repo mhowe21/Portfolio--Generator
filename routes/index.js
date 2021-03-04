0213be48db4569d124e971888452a6865247efd7
Mason Howe <mason.a.howe@gmail.com>
2021-03-04 16:10:09 -0700

    try rout update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const pageRoutes = require("./pages");
 
-router.use("", pageRoutes);
+router.use("/", pageRoutes);
 router.use("/api/v1/", apiRoutes);
 
 module.exports = router;
