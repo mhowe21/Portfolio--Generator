cee20330c08701a871fea6f242f1fc9f6a92ce2c
Mason Howe <mason.a.howe@gmail.com>
2021-03-04 15:20:34 -0700

    force sync

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 app.use(require("./routes"));
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log(`Live on port: ${PORT}`));
 });
