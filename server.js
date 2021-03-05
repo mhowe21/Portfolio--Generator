ff55bda719d0b308d3048ceff94bf109461d2d2a
Mason Howe <mason.a.howe@gmail.com>
2021-03-04 17:32:01 -0700

    none

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const exphbs = require("express-handlebars");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+//
+
 const sess = {
   secret: "Super secret secret",
   cookie: {},
